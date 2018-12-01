d8a39dd931118a972ea61fbfa32374d92844b531
rencild8 <rencild84@gmail.com>
2018-12-01 11:55:53 +0900

    Initial Commit

---
+++ TutorialView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TutorialView
+//
+//  Created by 佐藤優 on 2018/12/01.
+//  Copyright © 2018 佐藤優. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
